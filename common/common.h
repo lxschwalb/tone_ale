486a25cfbdd86cdd837a4a621071b1701586d8b7
Alex Schwalb <lxschwalb@gmail.com>
2023-01-23 00:44:58 +0100

    Update common.h

--- common/common.h
+++ common/common.h
@@ -5,8 +5,8 @@
  */
 void common_pins_setup();
 void common_capsense_setup();
-void common_i2cv_setup(int buffsize);
+void common_i2cv_setup(int32_t *buff, int buffsize, float samplerate);
 bool new_buff();
 int32_t * mutable_data();
-bool capsense_button();
+bool capsense_button(int threshold);
 void set_led(bool state);
