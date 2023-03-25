3a88d2a93c49ac3657d8f5ea7f0834a1233e807b
aleschw <aleschw@ini.uzh.ch>
2023-03-25 16:39:10 +0100

    added phaser

---
+++ phaser/phaser.cpp
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2023 Tone Age Technology
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include "pico/stdlib.h"
+#include "tone_ale.h"
+
+#define BUFFSIZE    16
+#define SAMPLE_RATE 48000
+#define SYSTEM_CLK  270000000
+
+#define PHASEBUFFSIZE   16
+#define NUM_PHASES      4
+#define FB_GAIN         0.13
+#define SPEED_Hz        2
+#define FLAVOUR         17
+
+constexpr int speed = SAMPLE_RATE/SIN_BUFFSIZE/SPEED_Hz;
+constexpr int phaseoffset = SIN_BUFFSIZE/NUM_PHASES/FLAVOUR;
+
+static int32_t leftbuff[PHASEBUFFSIZE] = {0};
+static int leftindex = PHASEBUFFSIZE/2;
+static int32_t rightbuff[PHASEBUFFSIZE] = {0};
+static int rightindex = 0;
+int modIndex = 0;
+
+static bool state = false;
+
+int32_t phaser(float input, int32_t* delaybuff, int* delayIndex)
+{
+    delaybuff[*delayIndex] = input;
+    int32_t output = 0;
+    for (int i = 0; i < NUM_PHASES; i++) {
+        int phaseShift = PHASEBUFFSIZE/2 + PHASEBUFFSIZE/2 * sin[(modIndex + phaseoffset * i)%SIN_BUFFSIZE];
+        output += delaybuff[(*delayIndex + phaseShift) % PHASEBUFFSIZE];
+    }
+
+    delaybuff[*delayIndex] += FB_GAIN * output;
+
+    *delayIndex = (*delayIndex+1) % PHASEBUFFSIZE;
+    
+    return (output/NUM_PHASES + input)/2;
+}
+
+void modulate() {
+    static int subdiv = 0;
+    subdiv++;
+
+    if(subdiv > speed) {
+        modIndex++;
+        subdiv = 0;
+    }
+
+    if(modIndex >= SIN_BUFFSIZE) {
+        modIndex = 0;
+    }
+}
+
+void interrupt_service_routine() {
+    juggle_buffers();
+
+    int32_t *buff = mutable_data();
+
+    if(state){
+        for (int i = 0; i < BUFFSIZE; i += 2) {
+            modulate();
+            buff[i] = conv_32bit_to_24_bit(phaser(correct_sign(buff[i]), leftbuff, &leftindex));
+            buff[i+1] = conv_32bit_to_24_bit(phaser(correct_sign(buff[i+1]), rightbuff, &rightindex));
+        }
+    }
+    else {
+        for(int i=0; i<BUFFSIZE; i++) {
+            buff[i] = buff[i] << 8;
+        }
+    }
+}
+
+int main() {
+    int32_t data_buff[BUFFSIZE*3];
+    set_sys_clock_khz(SYSTEM_CLK/1000, true);
+    tone_ale_pins_setup();
+    tone_ale_capsense_setup();
+    tone_ale_clk_setup(SAMPLE_RATE, SYSTEM_CLK);
+    tone_ale_i2cv_setup(data_buff, BUFFSIZE, interrupt_service_routine);
+
+    while (true) {
+        sleep_ms(1);
+        state = capsense_button(20);
+        set_led(state);
+    }
+}
